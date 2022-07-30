c3806c29db71f6339b2fbe2b6ff458d8f363bdec
hadid <100987221+Hadis199574@users.noreply.github.com>
2022-07-30 02:56:57 -0700

    Add files via upload

--- home_screen.dart
+++ home_screen.dart
@@ -2,8 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:searchbar_animation/searchbar_animation.dart';
 import 'package:untitled41/Screens/new_page.dart';
 
+import '../modles/money.dart';
+
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+   const HomeScreen({Key? key}) : super(key: key);
+  static List <Money> moneys= [ Money (
+  id: 0 ,
+  title:'test1',
+  data: "1400/01/01",
+  isReceived: true,
+    price: '1000',
+  ),
+    Money (
+      id: 1 ,
+      title:'test2',
+      data: "1400/02/01",
+      isReceived: true,
+      price: '2000',
+    ),
+    Money (
+      id: 2 ,
+      title:'test3',
+      data: "1400/03/01",
+      isReceived: false,
+      price: '4000',
+    ),
+  ];
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -52,15 +76,23 @@ class _HomeScreenState extends State<HomeScreen> {
                   ],
                 ),
               ),
-              const Spacer(),
-              Image.asset(
-                'assets/download.png',
-                width: 150,
-                height: 150,
-              ),
-              const Spacer(),
-              const SizedBox(height: 10.0),
-              const Text('تراکنشی موجود نیست! '),
+              // const Spacer(),
+              //Image.asset(
+              //  'assets/download.jpeg',
+              // width: 150,
+              //  height: 150,
+              //),
+              //  const Spacer(),
+              //const SizedBox(height: 10.0),
+              //  const Text('تراکنشی موجود نیست! '),
+             Expanded(
+               child: ListView.builder(itemCount: HomeScreen.moneys.length,
+
+                   itemBuilder: (context,indext){
+                 return MyListTilWidget(index: indext);
+               }
+               ),
+             )
             ],
           ),
         ),
@@ -68,3 +100,48 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+class MyListTilWidget extends StatelessWidget {
+  final int index;
+  const MyListTilWidget({Key? key,required this.index}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Row(
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+                color: Colors.pink,
+                borderRadius: BorderRadius.circular(15.0)),
+            child: const Center(
+              child: Icon(Icons.add, color: Colors.white, size: 30),
+            ),
+          ),
+           Padding(
+            padding:const EdgeInsets.only(left: 15.0),
+            child: Text(HomeScreen.moneys[index].title),
+          ),
+          const Spacer(),
+          Column(
+            children: [
+              Row(
+                children: [
+                  const Text(
+                    'تومان',
+                    style: TextStyle(color: Colors.pink),
+                  ),
+                  Text(HomeScreen.moneys[index].price,style: const TextStyle(color: Colors.pink),)
+                ],
+              ),
+              Text(HomeScreen.moneys[index].data),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
---
+++ money.dart
@@ -0,0 +1,10 @@
+
+class Money{
+  int id;
+  String price;
+  String data;
+  bool isReceived;
+  String title;
+  Money({ required this.id, required this.price,
+    required this.data, required this.isReceived, required this.title});
+}
